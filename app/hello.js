2bf0a3706f030f02aec0c87164c84b03185abd8c
drump <drump13@gmail.com>
2016-01-17 20:24:57 +0900

    hello

--- app/hello.js
+++ app/hello.js
@@ -3,11 +3,7 @@ function hello(name) {
 * ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
 * nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
 */
-	if(name == "World"){
-      return "Hello World!";
-    }else if(name == "codecheck"){
-      return "Hello codecheck!";
-    }
+    return "Hello "+ name;
   //return "Hello World!";
 }
 
