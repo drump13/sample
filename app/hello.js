64fc28ef5d95cc5b49b56525b9e42a6c871a5f94
u18484 <u18484@031b33a1-8859-4553-bcad-8e6c8798decb.prvt.dyno.rt.heroku.com>
2016-01-17 10:55:56 +0000

    Initial commit

---
+++ app/hello.js
@@ -0,0 +1,14 @@
+function hello(name) {
+/*
+* ここにnameに"World"が入力された時、この関数が"Hello World!"と返すような実装をします。
+* nameが"codecheck"の場合には、"Hello codecheck!"が返されます。
+*/
+	if(name == "World"){
+      return "Hello World!";
+    }else if(name == "codecheck"){
+      return "Hello codecheck!";
+    }
+  //return "Hello World!";
+}
+
+module.exports = hello;
